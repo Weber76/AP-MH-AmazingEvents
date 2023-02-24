8c64e1064323127100c52a1a3aa3b1248147ddff
Víctor Andrés Romano <victor.andres.romano@gmail.com>
2023-02-24 07:57:10 -0300

    past.html y upcoming.html con sus propios .js para mostrar eventos pasados y futuros. Modificado estilo del botón que lleva a detalles.

---
+++ js/upcoming.js
@@ -0,0 +1,30 @@
+let capturedElement = document.getElementById("main-article");
+
+let cards = ``;
+
+let currentDate = data.currentDate;
+
+for (let objectEvent of data.events) {
+  if (objectEvent.date >= currentDate) {
+    let cardTemplate = `
+        <div class="card">
+          <img
+            src="${objectEvent.image}"
+            class="card-img-top"
+            alt="${objectEvent.name}">
+          <div class="card-body">
+            <div>
+              <h5 class="card-title">${objectEvent.name}</h5>
+              <p class="card-text">${objectEvent.description}</p>
+            </div>
+            <div class="card-foot">
+                <p class="card-text">Price &#36;${objectEvent.price}</p>
+                <a href="#" class="btn btn-primary btn-card">View more...</a>
+            </div>
+          </div>
+        </div>`;
+    cards = cards + cardTemplate;
+  }
+}
+
+capturedElement.innerHTML = cards;
